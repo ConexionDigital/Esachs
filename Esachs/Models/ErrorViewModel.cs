68654c9f77254427dc96262e1af3d6f9bd21085c
desacta <dortiz@cta.cl>
2024-02-22 11:08:57 -0300

    Agregar archivos de proyecto.

---
+++ Esachs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esachs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
